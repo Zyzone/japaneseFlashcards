5f0c82c8014763f9b53ddb9981dd127cc12306df
Zyzone <Zyzone-@hotmail.com>
2018-05-12 13:24:24 -0500

    Implement flash card system for Katakana and Hirigana

--- JapaneseFlashcards/src/japaneseflashcards/JapaneseFlashcards.java
+++ JapaneseFlashcards/src/japaneseflashcards/JapaneseFlashcards.java
@@ -5,6 +5,9 @@
  */
 package japaneseflashcards;
 
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Scanner;
 /**
  *
  * @author Zyzone
@@ -15,8 +18,39 @@ public class JapaneseFlashcards {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello World");
-        System.out.println("はい");
+        ArrayList<String> List = new ArrayList<>();
+        ArrayList<String> Answers = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+        readFile(List, "Hiragana.txt");
+        readFile(Answers, "Hiragana Answers.txt");
+        
+        String str;
+        for(int i = 0; i < List.size(); i++) {
+            System.out.print(List.get(i) + ": ");
+            str = scanner.nextLine();
+            if(str.equals(Answers.get(i))) {
+                System.out.println("Corret! \n");
+            }
+            
+        }
     }
-    
+ 
+    public static void readFile(ArrayList<String> Strings, String file) {
+        try {
+            FileReader fr = new FileReader(file);
+            BufferedReader br = new BufferedReader(fr);
+            
+            String str;
+            while ((str = br.readLine()) != null) {
+                if(str.contains("あ") || str.contains("ア")) {
+                    Strings.add("" + str.charAt(1));                    
+                }else {
+                    Strings.add(str);
+                }
+            }
+            br.close();
+        }catch (IOException e) {
+            System.out.println("File not found");
+        }
+    }
 }
