e7a741fdbeebe0d289ffb49ed24cfa8705e178d9
Zyzone <Zyzone-@hotmail.com>
2018-05-22 18:25:14 -0500

    Create methods for previous code in main Add setFile method Add file list code to main

--- JapaneseFlashcards/src/japaneseflashcards/JapaneseFlashcards.java
+++ JapaneseFlashcards/src/japaneseflashcards/JapaneseFlashcards.java
@@ -7,6 +7,7 @@ package japaneseflashcards;
 
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 /**
  *
@@ -21,21 +22,22 @@ public class JapaneseFlashcards {
         ArrayList<String> List = new ArrayList<>();
         ArrayList<String> Answers = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
-        readFile(List, "Hiragana.txt");
-        readFile(Answers, "Hiragana Answers.txt");
         
-        String str;
-        for(int i = 0; i < List.size(); i++) {
-            System.out.print(List.get(i) + ": ");
-            str = scanner.nextLine();
-            if(str.equals(Answers.get(i))) {
-                System.out.println("Corret! \n");
-            }
-            
+        File folder = new File("C:\\Users\\Zyzone\\Git_Projects\\japaneseFlashcards\\JapaneseFlashcards");
+        File[] listOfFiles = folder.listFiles();
+
+        for (int i = 0; i < listOfFiles.length; i++) {
+            if (listOfFiles[i].isFile() && listOfFiles[i].toString().endsWith("txt")) {
+                System.out.println("File " + listOfFiles[i].getName());
+            } 
         }
+        
+        setFile(List, Answers, scanner);        
+        nextProblem(List, Answers, scanner);
     }
  
-    public static void readFile(ArrayList<String> Strings, String file) {
+    //Adds strings from file to the ArrayList Strings
+    public static boolean readFile(ArrayList<String> Strings, String file) {
         try {
             FileReader fr = new FileReader(file);
             BufferedReader br = new BufferedReader(fr);
@@ -49,8 +51,45 @@ public class JapaneseFlashcards {
                 }
             }
             br.close();
+            return true;
         }catch (IOException e) {
             System.out.println("File not found");
+            return false;
         }
+    }
+    
+    //Allows user to enter names for list and answers file
+    public static void setFile(ArrayList<String> list, ArrayList<String> answers, Scanner scanner) {
+        String str;
+        
+        while(true) {
+            System.out.print("Enter the name of the List file:");
+            str = scanner.nextLine();
+            if(readFile(list, str)) {
+                break;
+            }
+        }
+        
+        while(true) {
+            System.out.print("Enter the name of the Answer file:");
+            str = scanner.nextLine();
+            if(readFile(answers, str)) {
+                break;
+            } 
+        }
+    }
+    
+    //Gets the next problem from the list file; compares given answer to the answers file
+    public static void nextProblem(ArrayList<String> list, ArrayList<String> answers, Scanner scanner) {
+        String str;
+        
+        for(int i = 0; i < list.size(); i++) {
+            System.out.print(list.get(i) + ": ");
+            str = scanner.nextLine();
+            if(str.equals(answers.get(i))) {
+                System.out.println("Corret! \n");
+            }
+            
+        }   
     }
 }
